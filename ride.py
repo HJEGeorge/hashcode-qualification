19fcf5a6272ea50220a0ea880acf9405ab512116
Henry <hjegeorge@gmail.com>
2018-03-01 19:56:34 +0000

    Begun the main time sequence for simulation and created simple matching algorithm

--- ride.py
+++ ride.py
@@ -9,9 +9,13 @@ class Ride:
         self.finish = finish
         self.earliest_pickup = earliest_start
         self.latest_pickup = latest_finish - self.distance()
+        self.started = False
 
     def distance(self):
         return helper.distance(self.start, self.finish)
 
     def can_start(self, time):
-        return self.earliest_pickup <= time <= self.latest_pickup
+        return self.earliest_pickup <= time <= self.latest_pickup and not self.started
+
+    def start(self):
+        self.started = True
--- selfDrivingRides.py
+++ selfDrivingRides.py
@@ -1,5 +1,7 @@
 import driver
 import ride
+import sys
+import helper
 
 rides = []
 drivers = []
@@ -27,6 +29,33 @@ def main():
     Main function
     '''
     print(f'Running on file {sys.argv[1]}')
+
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} <input file> <out file>")
+        sys.exit()
+
+    print('Inputting from file %s' % sys.argv[1])
+
+    rows, columns, totalVehicles, totalRides, bonusPerRide, totalTime = read_file(sys.argv[1])
+
+    time = 0
+
+    for time in range(totalTime):
+        print(f"Current time = {time}")
+        available_drivers = list(filter(lambda x: x.isFree(time), drivers))
+        available_rides = list(filter(lambda x: x.can_start(time), rides))
+        for ride in available_rides:
+            if len(available_drivers) > 0:
+                driver = sorted(available_drivers, key=lambda x: helper.distance(x.currentLocation, ride.start))[0]
+                print(f"Driver {driver.id} has picked up ride {ride.id}")
+                driver.pick_up(ride)
+                ride.start()
+
+
+
+    print('Outputting to file %s' % sys.argv[2])
+
+
 
 
 if __name__ == '__main__':
