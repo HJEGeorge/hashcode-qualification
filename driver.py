1c2852f581943223cf3a1c8e4176471e3c3bab9a
Henry <hjegeorge@gmail.com>
2018-03-01 19:05:28 +0000

    Added Ride.py

--- driver.py
+++ driver.py
@@ -8,7 +8,7 @@ class Driver:
         self.willNextBeFree = 0
 
     def pick_up(self, time, ride):
-        # increase time to show the distance travelled
+        """increases time to show the distance travelled"""
         distance_to_ride = helper.distance(self.currentLocation, ride.start)
         self.willNextBeFree = time + distance_to_ride + ride.distance
 
---
+++ ride.py
@@ -0,0 +1,16 @@
+import helper
+
+
+class Ride:
+
+    def __init__(self, start, finish, earliest_start, latest_finish):
+        self.start = start
+        self.finish = finish
+        self.earliest_pickup = earliest_start
+        self.latest_pickup = latest_finish - self.distance()
+
+    def distance(self):
+        return helper.distance(self.start, self.finish)
+
+    def can_start(self, time):
+        return self.start < time < self.finish
